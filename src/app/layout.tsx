4fce91f48b8c9ee494407bb39047ce28126de0e8
VidulaDanthanarayana <158987027+VidulaDanthanarayana@users.noreply.github.com>
2025-09-01 18:10:44 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,6 @@ export const metadata: Metadata = {
     //   },
     // ],
   },
-  viewport: "width=device-width, initial-scale=1, maximum-scale=1",
 };
 
 export default function RootLayout({
@@ -37,7 +36,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
         <link href="https://fonts.cdnfonts.com/css/inter" rel="stylesheet" />
       </head>
       <body className={`bg-[#F6F7F9] antialiased`} suppressHydrationWarning>
